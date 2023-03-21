245d7a7a1e666f87c36160e2aef3b2e4eb368060
Squaria <Squaria@10.20.23.251>
2023-03-21 15:28:03 +0100

    test

---
+++ src/dao/ConnectionBdd.java
@@ -0,0 +1,10 @@
+package dao;
+
+public class ConnectionBdd {
+
+	//String URL = "jdbc:oracle:thin:@//srvoracledb.intranet.int:1521/orcl.intranet.
+	//String URL= "jdbc:oracle:thin:@//192.168.4.183:1521/orcl.intranet.int";
+	String URL = "jdbc:oracle:thin:@//oracle.esigelec.fr:1521/orcl.intranet.int";
+	String log = "C##_G7_G5_APP";
+	String pass = "APP_7_5";
+}
